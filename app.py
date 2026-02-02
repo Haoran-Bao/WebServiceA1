39b9215f9620dc7ead223768e3d491c769d77863
Haoran-Bao <albert_bao@hotmail.com>
2026-02-02 16:44:45 +0100

    main endpoints

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/<id>', methods=['GET'])
+def get_with_id(id):
+    pass
+
+
+@app.route('/<id>', methods=['PUT'])
+def put_with_id(id):
+    pass
+
+
+@app.route('/<id>', methods=['DELETE'])
+def delete_with_id(id):
+    pass
+
+
+@app.route('/', methods=['GET'])
+def get_all():
+    pass
+
+
+@app.route('/', methods=['POST'])
+def create_short_url():
+    pass
+
+@app.route('/', methods=['DELETE'])
+def delete_all():
+    pass
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
