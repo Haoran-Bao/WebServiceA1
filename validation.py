a08944217e45f20515e7a7b0021d66bf1c92721c
Harikrishna <h4hari2015@gmail.com>
2026-02-05 15:59:19 +0100

    updated regex

--- validation.py
+++ validation.py
@@ -7,8 +7,12 @@ def read_url_from_request(request) -> Optional[str]:
 
 
 def is_valid_url(url: str) -> bool:
-    url_pattern = "^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$"
-    return re.match(url_pattern, url) is not None
+        url_pattern = ("((http|https)://)(www.)?" + 
+             "[a-zA-Z0-9@:%._\\+~#?&//=]" + 
+             "{2,256}\\.[a-z]" + 
+             "{2,6}\\b([-a-zA-Z0-9@:%" + 
+             "._\\+~#?&//=]*)")
+        return re.match(url_pattern, url) is not None
 
 
 def normalize_url(url: str) -> str:
