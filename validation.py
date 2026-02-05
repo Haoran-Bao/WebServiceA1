1419ab1d5a20c8eef5b7319b99da8b501e329198
Harikrishna <h4hari2015@gmail.com>
2026-02-05 15:42:33 +0100

    Regex

--- validation.py
+++ validation.py
@@ -7,8 +7,8 @@ def read_url_from_request(request) -> Optional[str]:
 
 
 def is_valid_url(url: str) -> bool:
-    parsed = urllib.parse.urlparse(url)
-    return parsed.scheme in ['http', 'https'] and parsed.netloc != ''
+    url_pattern = "^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$"
+    return re.match(url_pattern, url) is not None
 
 
 def normalize_url(url: str) -> str:
