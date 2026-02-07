8b5371d13192cd4eed0e8c910c7faa17c2820728
Quincy <67159014+qu1ncyk@users.noreply.github.com>
2026-02-07 11:53:05 +0100

    Added MongoDB to the flake

--- flake.nix
+++ flake.nix
@@ -14,10 +14,26 @@
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
+        mongodb-bin = pkgs.stdenv.mkDerivation {
+          name = "mongodb-bin";
+          src = pkgs.fetchurl {
+            url = "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2404-8.2.4.tgz";
+            hash = "sha256-8yd+ehD83mvRSpZIEnEsK3hTrVEt8Mlp5nzhE/Hs/2g=";
+          };
+
+          nativeBuildInputs = [pkgs.autoPatchelfHook];
+          buildInputs = with pkgs; [curl libgcc openssl];
+
+          installPhase = ''
+            mkdir -p $out/bin
+            install -Dt $out/bin bin/mongod bin/mongos
+          '';
+        };
       in {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
-            (python3.withPackages (ps: [ps.flask ps.requests]))
+            (python3.withPackages (ps: [ps.flask ps.requests ps.pymongo]))
+            mongodb-bin
           ];
         };
       }
